e8dafc5bbad5c9968d6d2b3cebbc3e3733000e07
Snoopee <79824744+Snoopee110@users.noreply.github.com>
2025-01-14 05:47:10 +0000

    Update inventory.py

--- handlers/inventory.py
+++ handlers/inventory.py
@@ -3,17 +3,59 @@ from handlers.database import Database
 import discord
 
 class ItemEnums(Enum):
+    # Pet Items
     BasicPetFood = ('Basic Food', 0, 'Basic food for pets.')
+
+    #region property getters
+    @property
+    def name(self):
+        return self.value[0]
+
+    @property
+    def emoji(self):
+        return self.value[1]
+
+    @property
+    def description(self):
+        return self.value[2]
+    #endregion
 
 class Inventory:
     # Inventory Handler Class
     def __init__(self):
         self.db = Database()
 
-    async def get(self, ctx, user_id: int):
-        pass
+    def add_item(self, user_id: int, item: ItemEnums, amount: int):
+        user_data = self.db.universal_find_one('users', {'user_id': user_id})
+        if not user_data:
+            return
+        if item.name in user_data['inventory']:
+            user_data['inventory'][item.name] += amount
+        else:
+            user_data['inventory'][item.name] = amount
+        self.db.universal_update('users', {'user_id': user_id}, {'$set': user_data})
 
-    async def add_item(self, ctx, item: ItemEnums, user_id: int):
-        pass
+    def remove_item(self, user_id: int, item: ItemEnums, amount: int):
+        user_data = self.db.universal_find_one('users', {'user_id': user_id})
+        if not user_data:
+            return
+        if item.name in user_data['inventory']:
+            user_data['inventory'][item.name] -= amount
+        else:
+            return
+        self.db.universal_update('users', {'user_id': user_id}, {'$set': user_data})
 
-    
+    def get_inventory(self, user_id: int):
+        user_data = self.db.universal_find_one('users', {'user_id': user_id})
+        if not user_data:
+            return
+        return user_data['inventory']
+
+    def get_item(self, user_id: int, item: ItemEnums):
+        user_data = self.db.universal_find_one('users', {'user_id': user_id})
+        if not user_data:
+            return
+        if item.name in user_data['inventory']:
+            return user_data['inventory'][item.name]
+        else:
+            return 0
